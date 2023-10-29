ec5f44b033761ef326b6f60bdbf6285c9c5fc585
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-10-29 19:12:46 +0200

    render history transaction

--- 01/js/main.js
+++ 01/js/main.js
@@ -1,6 +1,4 @@
-
 /*
-
     Завдання: Система управління фінансами
     Вам потрібно створити систему управління фінансами, використовуючи JavaScript функції.
     Ваша програма повинна мати наступні можливості:
@@ -28,11 +26,10 @@
     Також, ви можете використовувати HTML та CSS для створення користувацького інтерфейсу.
 
     Бажаю успіху у вирішенні цього складного завдання на тему функцій у JavaScript!
-
  */
 
 
-/*---------- additional functions ----------*/
+/*---------- /additional functions ----------*/
 
 const randomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
@@ -40,84 +37,102 @@ const generate = (keyLength, characters) => [...Array(keyLength)]
     .map( () => characters[randomInteger(0, characters.length - 1)])
     .join('');
 
-const generateUniqueId = () => generate(9, 'abcdefghijklmnopqrstuvwxyz0123456789');
+const generateUniqueId = (length = 9) => generate(length, 'abcdefghijklmnopqrstuvwxyz0123456789');
 
 const formatDate = date => date.toLocaleDateString('uk-UA', { day: 'numeric', month: 'long', year: 'numeric' });
 
-const categories = ['utility payments', 'purchases', 'make transfer', 'receive transfer', 'replenishment'];
-const filterCondition = ['expenses', 'income', 'utility payments', 'purchases', 'make transfer', 'receive transfer', 'replenishment'];
-const getRandomItem = item => item[randomInteger(0, item.length - 1)];
+const getRandomArrayItem = array => array[randomInteger(0, array.length - 1)];
 
 /*---------- /additional functions ----------*/
 
 
-let transactions = [];
-let i = 11;
+const transactionService = () => {
+    const transactions = [];
+    const CATEGORIES = ['utility payments', 'purchases', 'make transfer', 'receive transfer', 'replenishment'];
+    const FILTER_CONDITION = ['expenses', 'income', 'utility payments', 'purchases', 'make transfer', 'receive transfer', 'replenishment'];
+    const TRANSACTION_FILTERS = {
+        'expenses': () => transactions.filter(a => a.amount < 0),
+        'income': () => transactions.filter(a => a.amount > 0),
+        'utility payments': () => transactions.filter(a => a.category === 'utility payments'),
+        'purchases': () => transactions.filter(a => a.category === 'purchases'),
+        'make transfer': () => transactions.filter(a => a.category === 'make transfer'),
+        'receive transfer': () => transactions.filter(a => a.category === 'receive transfer'),
+        'replenishment': () => transactions.filter(a => a.category === 'replenishment'),
+    };
 
-const addTransaction = (category, amount, comment = null) => {
-    transactions.push({
-        // id: generateUniqueId(),
-        id: i,
-        date: formatDate(new Date()),
-        category,
-        amount,
-        comment,
-    });
+    const add = (category, amount, comment = '') => {
+        transactions.push({
+            id: generateUniqueId(),
+            date: formatDate(new Date()),
+            category,
+            amount,
+            comment,
+        });
+    };
 
-    i++;
+    const remove = id => {
+        let idx = transactions.findIndex((item) => item.id === id);
+        idx !== -1  &&  transactions.splice(idx, 1);
+    };
+
+    const getTotalBalance = () => transactions.reduce((a, b) => a += b.amount, 0);
+
+    const save = () => {
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+    };
+
+    const load = () => {
+        transactions.concat(JSON.parse(localStorage.getItem('transactions')));
+    };
+
+    const findBy = (filterCondition) => TRANSACTION_FILTERS[filterCondition]();
+
+    const render = () => {
+        const tableBody = document.querySelector('#history tbody');
+        let str = '';
+
+        transactions.map((transaction, i) => {
+            str += `<tr>
+                        <td>${++i}</td>
+                        <td>${transaction.date}</td>
+                        <td>${transaction.amount}$</td>
+                        <td>${transaction.category}</td>
+                        <td>${transaction.comment}</td>
+                        <td><button type="button" class="btn btn-outline-danger">Remove</button></td>
+                    </tr>`
+        });
+
+        tableBody.innerHTML = str;
+    };
+
+    load();
+
+    return {
+        add,
+        save,
+        findBy,
+        remove,
+        getTotalBalance,
+        render,
+        getAll: () => transactions,
+        CATEGORIES,
+        FILTER_CONDITION,
+        TRANSACTION_FILTERS,
+    };
 };
 
-const deleteTransaction = id => {
-    transactions.forEach((item, index) => {
-        if (item.id === id) {
-            transactions.splice(index, 1);
-        }
-    });
-};
+const transactions = transactionService();
 
-const getTotalBalance = () => transactions.reduce((a, b) => a += b.amount, 0);
+transactions.add(getRandomArrayItem(transactions.CATEGORIES), randomInteger(-1000, 1000));
+transactions.add(getRandomArrayItem(transactions.CATEGORIES), randomInteger(-1000, 1000), 'for bread)');
+transactions.add(getRandomArrayItem(transactions.CATEGORIES), randomInteger(-1000, 1000), 'hello)');
+transactions.add(getRandomArrayItem(transactions.CATEGORIES), randomInteger(-1000, 1000));
+transactions.add(getRandomArrayItem(transactions.CATEGORIES), randomInteger(-1000, 1000), 'for coffee');
 
-const filteredTransactions = {
-    'expenses': () => transactions.filter(a => a.amount < 0),
-    'income': () => transactions.filter(a => a.amount > 0),
-    'utility payments': () => transactions.filter(a => a.category === 'utility payments'),
-    'purchases': () => transactions.filter(a => a.category === 'purchases'),
-    'make transfer': () => transactions.filter(a => a.category === 'make transfer'),
-    'receive transfer': () => transactions.filter(a => a.category === 'receive transfer'),
-    'replenishment': () => transactions.filter(a => a.category === 'replenishment'),
-};
+transactions.render();
 
-const getFilteredTransactions = (filterCondition) => filteredTransactions[filterCondition]();
+// console.log('Total Balance', transactions.getTotalBalance());
+// console.log('Filtered Transactions', transactions.findBy(getRandomArrayItem(transactions.FILTER_CONDITION)));
 
-const getTransactionsHistory = () => transactions.map((a, i) => console.log(`Transaction ${++i}: ${a.amount}$ - ${a.category}`));
-
-const setTransactions = (key, obj) => {
-    localStorage.setItem(key, JSON.stringify(obj));
-};
-
-const getTransactions = (key) => {
-    return JSON.parse(localStorage.getItem(key));
-};
-
-
-addTransaction(getRandomItem(categories), randomInteger(-1000, 1000));
-addTransaction(getRandomItem(categories), randomInteger(-1000, 1000), 'for bread)');
-addTransaction(getRandomItem(categories), randomInteger(-1000, 1000), 'hello)');
-addTransaction(getRandomItem(categories), randomInteger(-1000, 1000));
-addTransaction(getRandomItem(categories), randomInteger(-1000, 1000), 'for coffee');
-
-console.log(transactions);
-
-// deleteTransaction(12);
-
-console.log('Total Balance', getTotalBalance());
-
-console.log('Filtered Transactions', getFilteredTransactions(getRandomItem(filterCondition)));
-
-getTransactionsHistory();
-
-setTransactions('transactions', transactions);
-
-console.log('Get Transactions', getTransactions('transactions'));
-
-// localStorage.clear();
+// transactions.save();
+// console.log('Get Transactions', transactions.getAll());

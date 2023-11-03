ff43b3157c081df36a0f92ce7c737ed9e45d4922
Ruslan Pylypenko <r.pylypenko@m2e.team>
2023-11-03 10:51:50 +0200

    refactoring

--- js/transactionService.js
+++ js/transactionService.js
@@ -1,118 +1,55 @@
-/*
-    Завдання: Система управління фінансами
-    Вам потрібно створити систему управління фінансами, використовуючи JavaScript функції.
-    Ваша програма повинна мати наступні можливості:
-
-    Додавання та видалення транзакцій.
-    Створіть функцію, яка додає нову транзакцію з інформацією про суму та опис.
-    Створіть функцію, яка видаляє транзакцію за індексом.
-
-    Розрахунок загального балансу.
-    Створіть функцію, яка обчислює і повертає поточний баланс (сума всіх транзакцій).
-
-    Фільтрація транзакцій за певним критерієм.
-    Створіть функцію, яка приймає умову фільтрації (наприклад, всі транзакції з позитивними сумами) та повертає список транзакцій, що відповідають цій умові.
-
-    Відображення історії транзакцій.
-    Створіть функцію, яка виводить список транзакцій у зрозумілому форматі (наприклад, "Транзакція 1: +$100 - Покупка товару").
-
-    Збереження та завантаження даних.
-    Реалізуйте можливість зберігати поточний стан системи фінансів у локальному сховищі (localStorage)
-    та завантажувати дані при завантаженні сторінки.
-
-    Візуалізація даних.
-    Додайте можливість побудови графіка зміни балансу з часом.
-
-    Це завдання вимагає від вас використання функцій, масивів, обробників подій та роботи з локальним сховищем.
-    Також, ви можете використовувати HTML та CSS для створення користувацького інтерфейсу.
-
-    Бажаю успіху у вирішенні цього складного завдання на тему функцій у JavaScript!
- */
-
-
-/*---------- /additional functions ----------*/
-
-const randomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-
-const generate = (keyLength, characters) => [...Array(keyLength)]
-    .map( () => characters[randomInteger(0, characters.length - 1)])
-    .join('');
-
-const generateUniqueId = (length = 9) => generate(length, 'abcdefghijklmnopqrstuvwxyz0123456789');
-
-const formatDate = date => date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' });
-
-/*---------- /additional functions ----------*/
-
-const transactionService = () => {
-    const transactions = [];
-    const CATEGORIES = ['Salary', 'Transport', 'Food', 'Entertainment', 'Utility Payments'];
-    const TRANSACTION_FILTERS = {
-        'expenditure': () => transactions.filter(a => a.amount < 0),
-        'income': () => transactions.filter(a => a.amount > 0),
-        'salary': () => transactions.filter(a => a.category === 'salary'),
-        'transport': () => transactions.filter(a => a.category === 'transport'),
-        'food': () => transactions.filter(a => a.category === 'food'),
-        'entertainment': () => transactions.filter(a => a.category === 'entertainment'),
-        'utility payments': () => transactions.filter(a => a.category === 'utility payments'),
+const eventEmitter = () => {
+   const events = {}
+    return {
+        events,
+        subscribe: (eventName, callback) => {
+            !events[eventName] && (events[eventName] = []);
+            events[eventName].push(callback);
+        },
+        unsubscribe: (eventName, callback) => {
+            events[eventName] = events[eventName].filter(eventCallback => callback !== eventCallback);
+        },
+        emit: (eventName, args) => {
+            const event = events[eventName];
+            event && event.forEach(callback => callback.call(null, args));
+        }
     };
+}
 
-    /*
-   * Functions
-    */
+const tokenizer = () => {
+    const randomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+
+    const generate = (keyLength, characters) => [...Array(keyLength)]
+        .map( () => characters[randomInteger(0, characters.length - 1)])
+        .join('');
+
+    const generateUniqueId = (length = 9) => generate(length, 'abcdefghijklmnopqrstuvwxyz0123456789');
+
+    return {
+        randomInteger,
+        generateUniqueId,
+    }
+};
+
+const transactionsRepository = (tokenizer) => {
+    const transactions = [];
+    const load = () => localStorage.getItem('transactions') && transactions.push(...JSON.parse(localStorage.getItem('transactions')));
+
+    const getExpenses = () => transactions.filter(transaction => transaction.amount > 0);
 
     const getTotalBalance = () => transactions.reduce((a, b) => a += b.amount, 0);
+    const getTotalExpenses = () => getExpenses().reduce((a, b) => a += b.amount, 0);
 
-    const generateArr = () => {
-        const result = [['Category', 'Mhl']];
-        const getTotalBalance = () => transactions.reduce((a, b) => a += Math.abs(b.amount), 0);
-
-        for (let transaction of transactions) {
-            const amount = Math.abs(transaction.amount) / getTotalBalance() * 100;
-            const categoryExists = result.some(item => item[0] === transaction.category);
-
-            if (categoryExists) {
-                const index = result.findIndex(item => item[0] === transaction.category);
-                result[index][1] += amount;
-            } else {
-                result.push([transaction.category, amount]);
-            }
-        }
-
-        return result;
-    };
-
-    const generateArrToChart = () => {
-        const result = [];
-
-        transactions.map(transaction => {
-            const amount = transaction.amount < 0 && Math.abs(transaction.amount);
-            const date = transaction.date;
-
-            const dateExists = result.some(item => item.date === date);
-
-            if (dateExists) {
-                const index = result.findIndex(item => item.date === date);
-                result[index].balance += amount;
-            } else {
-                result.push({
-                    date: date,
-                    balance: amount
-                });
-            }
-        });
-
-        return result;
-    };
-
-    const add = (amount, category, comment) => {
+    const add = (amount, category, date, comment) => {
         transactions.push({
-            id: generateUniqueId(),
-            date: formatDate(new Date()),
+            id: tokenizer.generateUniqueId(),
+            date: date,
             amount: amount,
             category: category,
             comment: comment,
         });
+
+        save()
     };
 
     const remove = id => {
@@ -124,32 +61,61 @@ const transactionService = () => {
         localStorage.setItem('transactions', JSON.stringify(transactions));
     };
 
-    const findBy = (filterCondition) => TRANSACTION_FILTERS[filterCondition]();
+    const getAll = () => transactions;
 
-    /*
-    * Render
-     */
-
-    const load = () => {
-        localStorage.getItem('transactions') && transactions.push(...JSON.parse(localStorage.getItem('transactions')));
-    };
     load();
 
-    const renderCategorySelect = () => {
-        const categorySelect = document.querySelector('#categorySelect');
-        let str = '';
+    return{
+        getAll,
+        getExpenses,
+        remove,
+        add,
+        getTotalBalance,
+        getTotalExpenses,
+    }
+};
 
-        CATEGORIES.map(category => {
-            str += `<option>${category}</option>`
-        });
+const transactionService = (repository, events) => {
+    const CATEGORIES = ['Salary', 'Transport', 'Food', 'Entertainment', 'Utility Payments'];
 
-        categorySelect.innerHTML = str;
+    const getCategories = () => CATEGORIES
+
+    const add = (amount, category, date, comment) => {
+
+        if(CATEGORIES.find((item) => category) === undefined){
+            throw `Category ${category} is not found!`
+        }
+
+        repository.add(
+            amount,
+            category,
+            date || new Date(),
+            comment
+        )
+
+        events.emit('transaction.changed')
+        console.log(events)
+
+    }
+
+
+    const remove = () => {
+        repository.remove();
+        events.emit('transaction.changed')
+
+        console.log(events)
+    }
+
+    return {
+        add,
+        getCategories,
     };
-    renderCategorySelect();
+};
 
-    const renderTotalBalance = () => {
+const totalBalanceComponent = (repository, events) => {
+    const render = () => {
         const totalBalance = document.querySelector('#totalBalance');
-        const sum = getTotalBalance();
+        const sum = repository.getTotalBalance();
         let cl = 'green';
         if (sum < 0) {
             cl = 'red';
@@ -157,9 +123,79 @@ const transactionService = () => {
         totalBalance.innerHTML = `<h1 style="color:${cl}">${sum}$</h1>`
     };
 
-    const renderDiagram = () => {
+    events.subscribe('transaction.changed', () => render())
+
+    return {
+        render
+    }
+}
+
+const historyTableComponent = (transactionService, repository, events) => {
+    const removeItemEvent = () => {
+        const tableHistory = document.querySelector('#history');
+
+        tableHistory.addEventListener('click', e => {
+            e.preventDefault();
+
+            if (e.target && e.target.tagName === "BUTTON") {
+                let id = e.target.getAttribute('data-id');
+                transactionService.remove(id);
+            }
+        });
+    };
+    removeItemEvent();
+
+    const render = () => {
+        const tableBody = document.querySelector('#history tbody');
+        let str = '';
+
+        repository.getAll().map((transaction, i) => {
+            str += `<tr>
+                        <td>${++i}</td>
+                        <td>${transaction.amount}$</td>
+                        <td>${transaction.date}</td>
+                        <td>${transaction.category}</td>
+                        <td>${transaction.comment}</td>
+                        <td><button type="button" data-id="${transaction.id}" class="btn btn-outline-danger">Remove</button></td>
+                    </tr>`
+        });
+
+        tableBody.innerHTML = str;
+    };
+
+    events.subscribe('transaction.changed', () => render())
+
+
+    return {
+        render
+    }
+}
+
+const historyDiagramComponent = (repository, events) => {
+    const prepareData = () => {
+        const result = [['Category', 'Mhl']];
+        const totalExpenses = repository.getTotalExpenses()
+
+        for (let transaction of repository.getExpenses()) {
+            const amount = Math.abs(transaction.amount) / totalExpenses * 100;
+            const categoryExists = result.some(item => item[0] === transaction.category);
+
+            if (categoryExists) {
+                const index = result.findIndex(item => item[0] === transaction.category);
+                result[index][1] += amount;
+            } else {
+                result.push([transaction.category, amount]);
+            }
+        }
+
+        events.subscribe('transaction.changed', () => render())
+
+        return result;
+    };
+
+    const render = () => {
         const drawChart = () => {
-            const data = google.visualization.arrayToDataTable(generateArr());
+            const data = google.visualization.arrayToDataTable(prepareData());
 
             const chart = new google.visualization.PieChart(document.getElementById('myChart'));
             chart.draw(data);
@@ -168,11 +204,40 @@ const transactionService = () => {
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
     };
-    renderDiagram();
 
-    const renderChart = () => {
+    return {
+        render
+    }
+}
+
+const costScheduleComponent = (repository, events) => {
+    const prepareData = () => {
+        const result = [];
+
+        repository.getExpenses().map(transaction => {
+            const date = transaction.date;
+            const dateExists = result.some(item => item.date === date);
+            const amount = Math.abs(transaction.amount)
+
+            if (dateExists) {
+                const index = result.findIndex(item => item.date === date);
+                result[index].balance += amount;
+            } else {
+                result.push({
+                    date: formatDate(new Date(date)),
+                    balance: amount
+                });
+            }
+        });
+
+        return result;
+    };
+
+    events.subscribe('transaction.changed', () => render())
+
+    const render = () => {
         const ctx = document.getElementById('balanceChart').getContext('2d');
-        const data = generateArrToChart();
+        const data = prepareData();
 
         const dates = data.map(item => item.date);
         const balances = data.map(item => item.balance);
@@ -203,35 +268,30 @@ const transactionService = () => {
             }
         });
     };
-    renderChart();
 
-    const render = () => {
-        const tableBody = document.querySelector('#history tbody');
+    return {
+        render
+    }
+}
+
+const transactionFormComponent = (transactionService, totalBalance) => {
+    const renderCategorySelect = () => {
+        const categorySelect = document.querySelector('#categorySelect');
         let str = '';
 
-        transactions.map((transaction, i) => {
-            str += `<tr>
-                        <td>${++i}</td>
-                        <td>${transaction.amount}$</td>
-                        <td>${transaction.date}</td>
-                        <td>${transaction.category}</td>
-                        <td>${transaction.comment}</td>
-                        <td><button type="button" data-id="${transaction.id}" class="btn btn-outline-danger">Remove</button></td>
-                    </tr>`
+        transactionService.getCategories().map(category => {
+            str += `<option>${category}</option>`
         });
 
-        tableBody.innerHTML = str;
+        categorySelect.innerHTML = str;
     };
+    renderCategorySelect();
 
-    /*
-    * Events
-     */
 
-    const formSubmissionEvents = () => {
+    const handle = (transactionService) => {
         const newRecordForm = document.getElementById('newRecordForm');
         const newRecordBtn = document.getElementById('newRecordBtn');
-
-        newRecordBtn.addEventListener('click', function (e) {
+        newRecordForm.addEventListener('submit', function (e) {
             e.preventDefault();
 
             let category = document.getElementById('categorySelect').value;
@@ -239,66 +299,39 @@ const transactionService = () => {
             let amount = Math.abs(document.getElementById('amount').value);
             let comment = document.getElementById('comment').value;
             amount = type === 'income' ? amount : -amount;
-
             newRecordForm.reset();
 
-            // Add New Record
-            add(amount, category, comment);
-
-            // Rerender Transactions
-            render();
-
-            // Rerender Total Balance
-            renderTotalBalance();
-
-            // Save To LocalStorage
-            save();
-
-            // Load Diagram
-            renderDiagram();
+            transactionService.add(amount, category, null, comment);
         });
-    };
-    formSubmissionEvents();
-
-    const removeItemEvents = () => {
-        const tableHistory = document.querySelector('#history');
-
-        tableHistory.addEventListener('click', e => {
-            e.preventDefault();
-
-            if (e.target && e.target.tagName === "BUTTON") {
-                let id = e.target.getAttribute('data-id');
-
-                // Remove record
-                remove(id);
-
-                // Rerender Transactions
-                render();
-
-                // Render Total Balance
-                renderTotalBalance();
-
-                // Save To LocalStorage
-                save();
-
-                // Load Diagram
-                renderDiagram();
-            }
-        });
-    };
-    removeItemEvents();
-
-
+    }
     return {
-        renderTotalBalance,
-        render
-    };
-};
+        handle: () => handle(transactionService, totalBalance)
+    }
+}
 
-const transactions = transactionService();
+/*---------- /additional functions ----------*/
+const formatDate = date => date.toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' });
 
-// Render Total Balance
-transactions.renderTotalBalance();
 
-// Render Transactions
-transactions.render();
+/*---------- /additional functions ----------*/
+
+const events = eventEmitter();
+const repository = transactionsRepository(tokenizer());
+const transaction = transactionService(repository, events);
+
+const totalBalance = totalBalanceComponent(repository, events)
+const historyTable = historyTableComponent(transaction, repository, events)
+const historyDiagram = historyDiagramComponent(repository, events)
+const costSchedule = costScheduleComponent(repository, events)
+
+const transactionForm = transactionFormComponent(transaction, totalBalance)
+
+
+totalBalance.render()
+transactionForm.handle()
+historyTable.render()
+historyDiagram.render()
+costSchedule.render()
+
+console.log(events)
+

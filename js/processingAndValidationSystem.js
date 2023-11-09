f76934fcb3448916f124bdc9ae2e670d70b391a2
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-11-09 10:39:34 +0200

    feedback form

--- js/processingAndValidationSystem.js
+++ js/processingAndValidationSystem.js
@@ -24,19 +24,19 @@ class FeedbackForm {
         return feedbackForm.innerHTML = `
                 <div class="mb-3">
                     <label for="name" class="form-label">Name:</label>
-                    <input type="text" class="form-control" id="name" placeholder="Enter name" name="name" required>
+                    <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" required>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                 </div>
                 <div class="mb-3">
                     <label for="subject" class="form-label">Message subject:</label>
-                    <input type="text" class="form-control" id="subject" placeholder="Enter subject" name="subject" required>
+                    <input type="text" class="form-control" id="subject" placeholder="Enter subject" name="subject">
                 </div>
                 <div class="mb-3">
                     <label for="comment" class="form-label">Comment:</label>
-                    <textarea class="form-control" rows="2" id="comment" name="text" required></textarea>
+                    <textarea class="form-control" rows="2" id="comment" name="text"></textarea>
                 </div>
                 <div class="mb-3">
                     <button type="submit" id="submitButton" class="btn btn-outline-success">Submit</button>
@@ -45,27 +45,48 @@ class FeedbackForm {
     }
 
     collectData() {
-        const data = [];
-
         let name = document.getElementById('name').value;
         let email = document.getElementById('email').value;
         let subject = document.getElementById('subject').value;
         let comment = document.getElementById('comment').value;
 
-        data.push({
-            name: name,
-            email: email,
-            subject: subject,
-            comment: comment,
-        });
-
-        return data;
+        return {
+            name,
+            email,
+            subject,
+            comment,
+        };
     }
 }
 
 class FormValidator {
     validate(data) {
-        return data;
+        const validationErrors = {};
+
+        if (data.name.trim() === '') {
+            validationErrors.name = 'Name is a required field!';
+        }
+
+        if (data.email.trim() === '') {
+            validationErrors.email = 'Email is a required field!';
+        } else if (!this.#isValidEmail(data.email)) {
+            validationErrors.email = 'The email is in the wrong format!';
+        }
+
+        if (data.subject.trim() === '') {
+            validationErrors.subject = 'Subject is a required field!';
+        }
+
+        if (data.message && data.message.trim().length <= 20) {
+            validationErrors.message = 'The text of the message must contain at least 20 characters!';
+        }
+
+        return validationErrors;
+    }
+
+    #isValidEmail(email) {
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return emailRegex.test(email);
     }
 }
 
@@ -82,16 +103,21 @@ const formValidator = new FormValidator();
 const feedbackProcessor = new FeedbackProcessor();
 
 const form = document.querySelector('#feedbackForm');
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const collectData = feedbackForm.collectData();
     console.log(collectData);
 
-    formValidator.validate(collectData);
-    feedbackProcessor.process(collectData);
+    const validationErrors = formValidator.validate(collectData);
+    console.log(validationErrors);
+
+    if(Object.keys(validationErrors).length === 0) {
+        feedbackProcessor.process(collectData);
+        alert('Success: Data saved successfully!');
+    } else {
+        alert('Error: Enter the data again!');
+    }
 
     form.reset();
 });
-
-

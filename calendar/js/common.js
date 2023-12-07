c847034ca3733d72077e0915f376266dc3269f52
NataliaPylypenko <natalia28ivanivna@gmail.com>
2023-12-07 14:35:37 +0200

    generate current date

--- calendar/js/common.js
+++ calendar/js/common.js
@@ -1,18 +1,17 @@
 class Calendar {
     constructor(element) {
         this.element = element;
-        this.weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
     }
 
-    generateCalendar() {
+    generateCalendar(currentDate, daysOfTheWeek) {
       return `
-        <div class="current-date">
-          <h1>Friday 15th</h1>
-          <h1>January 2016</h1>
+        <div class="currentDate">
+          <h1>${currentDate.day} ${currentDate.number}</h1>
+          <h1>${currentDate.month} ${currentDate.year}</h1>
         </div>
 
-        <div class="current-month">
-          <ul class="week-days">${this.weekDays.map(day => `<li>${day}</li>`).join('')}</ul>
+        <div class="daysOfTheWeek">
+          <ul class="week-days">${daysOfTheWeek.map(day => `<li>${day}</li>`).join('')}</ul>
         </div>
             
         <div class="weeks">
@@ -29,8 +28,8 @@ class Calendar {
       `
     }
 
-    render() {
-        this.element.innerHTML = this.generateCalendar();
+    render(currentDate, daysOfTheWeek) {
+        this.element.innerHTML = this.generateCalendar(currentDate, daysOfTheWeek);
     }
 }
 
@@ -39,10 +38,30 @@ class DateSelector {}
 class AppController {
     constructor(calendar) {
         this.calendar = calendar;
+        this.weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        this.month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     }
 
     initialize() {
-        this.calendar.render();
+        this.calendar.render(this.getCurrentDate(), this.daysOfTheWeek());
+    }
+
+    getCurrentDate() {
+        let date = new Date();
+        return {
+            day: this.weekDays[date.getDay() === 0 ? 6 : date.getDay() - 1],
+            number: `${this.addZero(date.getDate())}th`,
+            month: this.month[date.getMonth()],
+            year: date.getFullYear()
+        }
+    }
+
+    addZero(num) {
+        return num > 0 && num < 10 ? '0' + num : num;
+    }
+
+    daysOfTheWeek() {
+        return this.weekDays.map(day => day.substr(0, 3).toUpperCase())
     }
 }
 
